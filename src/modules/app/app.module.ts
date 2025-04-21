daba4c51b6e4057790d9f4a5e2749f15be5f3f28
Aleksei Moskalenko <unwr22w@Alekseis-MacBook-Pro-2.local>
2025-04-21 15:49:06 +0400

    fix: minor

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { LoggerModule, Params } from 'nestjs-pino'
 import { TradingModule } from '../trading/module'
-import { appConfig } from './infrastructure/config/config'
+import { APP_CONFIG_KEY, appConfig } from './infrastructure/config/config'
 import { AppConfig } from './infrastructure/config/config.types'
 
 @Module({
@@ -11,7 +11,7 @@ import { AppConfig } from './infrastructure/config/config.types'
     LoggerModule.forRootAsync({
       imports: [ConfigModule.forFeature(appConfig)],
       useFactory(configService: ConfigService): Params {
-        const config = configService.getOrThrow<AppConfig>('app')
+        const config = configService.getOrThrow<AppConfig>(APP_CONFIG_KEY)
         return {
           pinoHttp: {
             level:    config.logLevel,
